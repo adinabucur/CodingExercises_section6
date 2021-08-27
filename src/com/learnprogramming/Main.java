d4b440243df3377b1e5c9e69f6e2b45e275f824a
Adina Bucur <45669764+adinabucur@users.noreply.github.com>
2021-08-27 20:07:00 +0300

    Update

--- src/com/learnprogramming/Main.java
+++ src/com/learnprogramming/Main.java
@@ -3,17 +3,35 @@ package com.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-        SimpleCalculator calculator = new SimpleCalculator();
-        calculator.setFirstNumber(5.0);
-        calculator.setSecondNumber(4);
-        System.out.println("add= " + calculator.getAdditionResult());
-        System.out.println("subtract= " + calculator.getSubtractionResult());
+//        SimpleCalculator calculator = new SimpleCalculator();
+//        calculator.setFirstNumber(5.0);
+//        calculator.setSecondNumber(4);
+//        System.out.println("add= " + calculator.getAdditionResult());
+//        System.out.println("subtract= " + calculator.getSubtractionResult());
+//
+//
+//        calculator.setFirstNumber(5.25);
+//        calculator.setSecondNumber(0);
+//        System.out.println("multiply= " + calculator.getMultiplicationResult());
+//        System.out.println("divide= " + calculator.getDivisionResult());
 
+//        Wall wall = new Wall(5, 4);
+//        System.out.println("area= " + wall.getArea());
+//
+//        wall.setHeight(-1.5);
+//        System.out.println("width= " + wall.getWidth());
+//        System.out.println("height= " + wall.getHeight());
+//        System.out.println("area= " + wall.getArea());
 
-        calculator.setFirstNumber(5.25);
-        calculator.setSecondNumber(0);
-        System.out.println("multiply= " + calculator.getMultiplicationResult());
-        System.out.println("divide= " + calculator.getDivisionResult());
+        Carpet carpet = new Carpet(3.5);
+        Floor floor = new Floor(2.75, 4.00);
+        Calculator calculator = new Calculator(floor, carpet);
+        System.out.println("total= " + calculator.getTotalCost());
+
+        carpet = new Carpet(1.5);
+        floor = new Floor(5.4, 4.5);
+        calculator = new Calculator(floor, carpet);
+        System.out.println("total= " + calculator.getTotalCost());
 
     }
 }
