8b191faec7040bef093a57dce80b7f36c3e9af0d
Adina Bucur <45669764+adinabucur@users.noreply.github.com>
2021-08-29 13:16:37 +0300

    Update

--- src/com/learnprogramming/Main.java
+++ src/com/learnprogramming/Main.java
@@ -23,15 +23,35 @@ public class Main {
 //        System.out.println("height= " + wall.getHeight());
 //        System.out.println("area= " + wall.getArea());
 
-        Carpet carpet = new Carpet(3.5);
-        Floor floor = new Floor(2.75, 4.00);
-        Calculator calculator = new Calculator(floor, carpet);
-        System.out.println("total= " + calculator.getTotalCost());
+//        Carpet carpet = new Carpet(3.5);
+//        Floor floor = new Floor(2.75, 4.00);
+//        Calculator calculator = new Calculator(floor, carpet);
+//        System.out.println("total= " + calculator.getTotalCost());
+//
+//        carpet = new Carpet(1.5);
+//        floor = new Floor(5.4, 4.5);
+//        calculator = new Calculator(floor, carpet);
+//        System.out.println("total= " + calculator.getTotalCost());
 
-        carpet = new Carpet(1.5);
-        floor = new Floor(5.4, 4.5);
-        calculator = new Calculator(floor, carpet);
-        System.out.println("total= " + calculator.getTotalCost());
+//        Circle circle = new Circle(3.75);
+//        System.out.println("circle.radius= " + circle.getRadius());
+//        System.out.println("circle.area= " + circle.getArea());
+//        Cylinder cylinder = new Cylinder(5.55, 7.25);
+//        System.out.println("cylinder.radius = " + cylinder.getRadius());
+//        System.out.println("cylinder.height = " + cylinder.getHeight());
+//        System.out.println("cylinder.area = " + cylinder.getArea());
+//        System.out.println("cylinder.volume = " + cylinder.getVolume());
+
+        Rectangle rectangle = new Rectangle(5, 10);
+        System.out.println("rectangle.width= " + rectangle.getWidth());
+        System.out.println("rectangle.length= " + rectangle.getLength());
+        System.out.println("rectangle.area= " + rectangle.getArea());
+        Cuboid cuboid = new Cuboid(5, 10, 5);
+        System.out.println("cuboid.width= " + cuboid.getWidth());
+        System.out.println("cuboid.length= " + cuboid.getLength());
+        System.out.println("cuboid.area= " + cuboid.getArea());
+        System.out.println("cuboid.height= " + cuboid.getHeight());
+        System.out.println("cuboid.volume= " + cuboid.getVolume());
 
     }
 }
